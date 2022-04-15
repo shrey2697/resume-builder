38e1b62d90ed7cd6ae17a45090b75d3b763183cc
shrey2697 <shrey2697@gmail.com>
2022-04-15 14:46:43 +0530

    feat: added print button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { Content } from "./components/Content";
 import "./App.css";
 import { SidePanel } from "./components/SidePanel/SidePanel";
+// import html2canvas from "html2canvas";
+// import jsPDF from "jspdf";
 
 function App() {
+  //TODO:
+  // function print() {
+  //   const input = document.getElementById("resume");
+  //   html2canvas(input).then((canvas) => {
+  //     const imgData = canvas.toDataURL("image/png");
+  //     const pdf = new jsPDF();
+  //     pdf.addImage(imgData, "JPEG", 0, 0);
+  //     // pdf.output('dataurlnewwindow');
+  //     pdf.save("download.pdf");
+  //   });
+  // }
   return (
     <div className="App">
-      <div className="Resume">
+      <button id="download" onClick={() => window.print()}>
+        DOWNLOAD RESUME
+      </button>
+      <div className="Resume" id="resume">
         <SidePanel />
         <Content />
       </div>
